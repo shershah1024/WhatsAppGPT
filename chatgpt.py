efa8e39c2f42fd011b833d78a438969b805704a2
shershah1024 <125534182+shershah1024@users.noreply.github.com>
2023-03-11 04:02:11 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -2,6 +2,14 @@ import os
 import sys
 import time
 import openai
+from dotenv import load_dotenv
+
+
+load_dotenv()
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+twilio_phone_number = os.environ.get('TWILIO_PHONE_NUMBER')
 
 systemPrompt = { "role": "system", "content": "You are a helpful assistant." }
 
